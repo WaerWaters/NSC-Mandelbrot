cbd414906b916c1ddc6732da1ef62a3337bf5393
WaerWaters <daniel.johnsen1@hotmail.com>
2026-02-06 10:34:40 +0100

    loop through grid once instead of twice

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,36 +1,22 @@
 import numpy
 import matplotlib.pyplot as plt
 
-xmin = -2
-xmax = 1
-ymin = -1.5
-ymax = 1.5
-
-width = 500
-height = 500
-
-
-complex_numbers = []
-
-for i in numpy.linspace(xmin, xmax, width):
-    temp = []
-    for j in numpy.linspace(ymin, ymax, height):
-        c = complex(i,j)
-        temp.append(c)
-    complex_numbers.append(temp)
-
-
-saved_iter = []
+xmin, xmax = -2, 1
+ymin, ymax = -1.5, 1.5
+width, height = 500, 500
 max_iter = 100
 
-for row in complex_numbers:
+saved_iter = []
+
+for x in numpy.linspace(xmin, xmax, width):
     temp = []
-    for i in row:
+    for y in numpy.linspace(ymin, ymax, height):
+        c = complex(x,y)
         z = 0
-        for j in range(max_iter):
-            z = z**2 + i
+        for i in range(max_iter):
+            z = z**2 + c
             if abs(z) > 2:
-                temp.append(j)
+                temp.append(i)
                 break
         else:
             temp.append(max_iter)
