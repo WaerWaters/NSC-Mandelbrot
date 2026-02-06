801fef327db2c7c2c74a8d2fcb062aded04b4eaa
WaerWaters <daniel.johnsen1@hotmail.com>
2026-02-06 10:53:20 +0100

    Added execution time and figure save

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,27 +1,36 @@
 import numpy
 import matplotlib.pyplot as plt
+import time
 
-xmin, xmax = -2, 1
-ymin, ymax = -1.5, 1.5
-width, height = 500, 500
-max_iter = 100
 
-saved_iter = []
+def compute_mandelbrot(xmin, xmax, ymin, ymax, width, height, max_iter, display):
+    saved_iter = []
+    for x in numpy.linspace(xmin, xmax, width):
+        temp = []
+        for y in numpy.linspace(ymin, ymax, height):
+            c = complex(x,y)
+            z = 0
+            for i in range(max_iter):
+                z = z**2 + c
+                if abs(z) > 2:
+                    temp.append(i)
+                    break
+            else:
+                temp.append(max_iter)
+        saved_iter.append(temp)
 
-for x in numpy.linspace(xmin, xmax, width):
-    temp = []
-    for y in numpy.linspace(ymin, ymax, height):
-        c = complex(x,y)
-        z = 0
-        for i in range(max_iter):
-            z = z**2 + c
-            if abs(z) > 2:
-                temp.append(i)
-                break
-        else:
-            temp.append(max_iter)
-    saved_iter.append(temp)
+    plt.imshow(saved_iter, cmap="hot")
+    plt.title("Mandelbrot Set")
+    plt.colorbar()
+    plt.savefig("mandelbrot.png")
+    if display:
+        plt.show()
+   
+    return saved_iter
 
-plt.imshow(saved_iter)
-plt.show()
+
+start = time.time()
+results = compute_mandelbrot(-2, 1, -1.5, 1.5, 1024, 1024, 100, display=False)
+elapsed = time.time() - start
+print(f"computation took {elapsed:.3f} seconds")
 
